c13f0a44ea0f081080b71a9f06506e797efa48f5
perchopick <ivanhuay@gmail.com>
2017-10-13 15:32:19 -0300

    Generate wallet batch bytes working

---
+++ example/createWalletBatchBytes.js
@@ -0,0 +1,29 @@
+'use strict';
+const SawtoothSdk = require('./lib/sawtooth-sdk');
+const sdk = new SawtoothSdk({
+    dependencies: [],
+    familyVersion: '1.0',
+    payloadEncoding: 'application/protobuf',
+    privateKey: '65df52d50dc235f904872d0b7f14ac1517a267c9a9d49d928e9f3071db1b27bd'
+});
+
+
+const SAWTOOTH_PAYMENT_PROCESSOR_FAMILY = 'exoexample';
+const EXO_SP_NAMESPACE = sdk.hash(SAWTOOTH_PAYMENT_PROCESSOR_FAMILY).substring(0, 6);
+const fromAddress = EXO_SP_NAMESPACE + sdk.hash(sdk.wallet.getPublic()).slice(0, 64);
+
+const payload = {
+    public_key: sdk.wallet.getPublic(),
+    type: 0
+};
+const batchBytesWallet = sdk.generateWalletBatchBytes(fromAddress, payload, {
+    batcherPubkey: sdk.wallet.getPublic(),
+    dependencies: [],
+    familyName: SAWTOOTH_PAYMENT_PROCESSOR_FAMILY,
+    familyVersion: '1.0',
+    inputs: [EXO_SP_NAMESPACE],
+    outputs: [EXO_SP_NAMESPACE],
+    payloadEncoding: 'application/protobuf'
+});
+
+console.log('batchBytesWallet: ', batchBytesWallet);
